286e759fcbe04c5efbeca15d7a82877d7a15189c
Anne Ogongo <anneogongo98@gmail.com>
2023-02-16 21:10:06 +0300

    complete

--- pythonfetch.py
+++ pythonfetch.py
@@ -1,13 +1,11 @@
 import requests
 
-# for item in range(10):
 response = requests.get('https://randomuser.me/api/?results=100&gender=male')
 
 data = response.json()
 
 male_users = data['results']
 
-# print(male_users)
 
 for i, user in enumerate(male_users):
 
